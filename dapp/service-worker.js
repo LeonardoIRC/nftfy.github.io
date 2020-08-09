5e581992e231635ecab4205864a47638c3af64e3
Fabricio Miranda <fabriciomirandabr@gmail.com>
2020-08-09 19:20:42 -0300

    Update

--- dapp/service-worker.js
+++ dapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dapp/precache-manifest.f0750e13f3edb196911f327892e2a075.js"
+  "/dapp/precache-manifest.f7a21551856498e8bbab6e8f62bd87dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
